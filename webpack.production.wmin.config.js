f4eb556b724517ac7c9472eeed7d2ca606ffe541
N0taN3rd <n0tan3rd@gmail.com>
2017-03-01 00:54:07 -0500

    adding mins

---
+++ webpack.production.wmin.config.js
@@ -0,0 +1,74 @@
+const {resolve} = require('path')
+const webpack = require('webpack')
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: '[name]-bundle.js',
+    chunkFilename: '[name]-chunk.js',
+    path: resolve(__dirname, 'minifiedDist'),
+
+    publicPath: '/'
+    // necessary for HMR to know where to load the hot update chunks
+  },
+
+  context: resolve(__dirname, 'src'),
+
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {loader: "style-loader"},
+          {
+            loader: "css-loader"
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ico)$/,
+        loader: 'url-loader?limit=10000',
+      }, {
+        test: /\.json$/,
+        loader: 'json-loader',
+      }, {
+        test: /\.(eot|ttf|wav|mp3|tex)$/,
+        loader: 'file-loader',
+      }, {
+        test: /\.(txt|xml|cxml)$/,
+        loader: 'raw-loader',
+      }
+    ],
+  },
+  plugins: [
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'node-static',
+      minChunks(module, count) {
+        const context = module.context
+        return context && context.indexOf('node_modules') >= 0
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      compress: {
+        warnings: false,
+        drop_console: false,
+      }
+    }),
+    new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    // new BundleAnalyzerPlugin({
+    //   analyzerMode: 'static'
+    // }),
+  ],
+}
